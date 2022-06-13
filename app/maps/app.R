dbafb18426bd685ce7d7ede6cf3108001135a20f
killian31 <80256683+killian31@users.noreply.github.com>
2022-06-13 16:41:27 +0200

    Add files via upload

---
+++ app/maps/app.R
@@ -0,0 +1,4 @@
+### https://rviews.rstudio.com/2019/10/09/building-interactive-world-maps-in-shiny/ ###
+
+### https://github.com/fverkroost/RStudio-Blogs/blob/master/interactive_worldmap_shiny_app.R ###
+### line 257 ###
